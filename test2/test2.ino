27a8ef4719cd7d4ec756928091649981015dbe22
DavidIoner <davidhioner@gmail.com>
2022-07-23 22:50:18 -0300

    http update

--- test2/test2.ino
+++ test2/test2.ino
@@ -1,110 +1,107 @@
-// Inclusão das Bibliotecas
-#include <ESP8266WiFi.h> 
-// #include <PubSubClient.h>
-#include <Wire.h>
-// #include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+// #include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
 
-// Endereco I2C do sensor MPU-6050
-const int MPU = 0x68;
+HTTPClient httpClient;
+WiFiClient client;
+// versao atual do firmware
+// const int FW_VERSION = 1244;
+// Url do firmware
+const char* fwUrlBase = "http://storage.googleapis.com";
 
-const int sda_pin = D5; // definição do pino I2C SDA
-const int scl_pin = D6; // definição do pino I2C SCL
+const char* ssid = "Orion*Fibra_David";
+const char* password = "ioner004";
 
-// Variaveis para armazenar valores do sensor
-float AccX, AccY, AccZ, Temp, GyrX, GyrY, GyrZ;
-
-void initMPU() {
-  Wire.begin(sda_pin, scl_pin);
-  Wire.beginTransmission(MPU);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-
-  // Configura Giroscópio para fundo de escala desejado
-  /*
-    Wire.write(0b00000000); // fundo de escala em +/-250°/s
-    Wire.write(0b00001000); // fundo de escala em +/-500°/s
-    Wire.write(0b00010000); // fundo de escala em +/-1000°/s
-    Wire.write(0b00011000); // fundo de escala em +/-2000°/s
-  */
-  Wire.beginTransmission(MPU);
-  Wire.write(0x1B);
-  Wire.write(0x00011000);  // Trocar esse comando para fundo de escala desejado conforme acima
-  Wire.endTransmission();
-
-  // Configura Acelerometro para fundo de escala desejado
-  /*
-      Wire.write(0b00000000); // fundo de escala em +/-2g
-      Wire.write(0b00001000); // fundo de escala em +/-4g
-      Wire.write(0b00010000); // fundo de escala em +/-8g
-      Wire.write(0b00011000); // fundo de escala em +/-16g
-  */
-  Wire.beginTransmission(MPU);
-  Wire.write(0x1C);
-  Wire.write(0b00011000);  // Trocar esse comando para fundo de escala desejado conforme acima
-  Wire.endTransmission();
+void setWifi(){
+  Serial.println();      
+  WiFi.begin(ssid, password); //Inicia WiFi 
+  delay(500);
+  Serial.print("Conectando"); 
+  while (WiFi.status() != WL_CONNECTED) // Tentando conectar na Rede WiFi
+  {
+    delay(500);
+    Serial.print(".");
+   }
+  Serial.println();
+  
+  Serial.print("Conectado | Endereço IP: "); 
+  Serial.println(WiFi.localIP()); //Imprime o Endereço IP Local do ESP8266
 }
 
-void readData() {
-  // Comandos para iniciar transmissão de dados
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 14, true); // Solicita os dados ao sensor
+void checkForUpdates() {
+  String mac = WiFi.macAddress();
+  String fwURL = String( fwUrlBase );
+  fwURL.concat( mac );
+  // String fwVersionURL = fwURL;
+  // fwVersionURL.concat( ".version" );
+  Serial.println( "Checking for firmware updates." );
+  Serial.print( "MAC address: " );
+  // Serial.println( mac );
+  // Serial.print( "Firmware version URL: " );
+  // Serial.println( fwVersionURL );
 
-  // Armazena o valor dos sensores nas variaveis correspondentes
-  AccX = Wire.read() << 8 | Wire.read(); //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-  AccY = Wire.read() << 8 | Wire.read(); //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AccZ = Wire.read() << 8 | Wire.read(); //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Temp = Wire.read() << 8 | Wire.read(); //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyrX = Wire.read() << 8 | Wire.read(); //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyrY = Wire.read() << 8 | Wire.read(); //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyrZ = Wire.read() << 8 | Wire.read(); //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  // httpClient.begin( client, fwVersionURL );
+  // apiClient.begin("https://storage.googleapis.com/sensor-version/downtime/firmware.bin");
+  httpClient.begin( client, "http://storage.googleapis.com/sensor-version/downtime/firmware.bin" );
+  int httpCode = httpClient.GET();
+  if( httpCode == 200 ) {
+    String newFWVersion = httpClient.getString();
+    Serial.println(newFWVersion);
+
+    // Serial.print( "Current firmware version: " );
+    // Serial.println( FW_VERSION );
+    // Serial.print( "Available firmware version: " );
+    // Serial.println( newFWVersion );
+
+    // int newVersion = newFWVersion.toInt();
+
+    // if( newVersion > FW_VERSION ) 
+    
+    if( 1==1 ){
+      Serial.println( "Preparing to update" );
+
+      // String fwImageURL = fwURL;
+      // fwImageURL.concat( ".bin" );
+      // update(client, host (httpserver), port, uri)
+      t_httpUpdate_return ret = ESPhttpUpdate.update( client, "https://storage.googleapis.com/sensor-version/downtime", 80, "/firmware.bin" );
+
+      switch(ret) {
+        case HTTP_UPDATE_FAILED:
+          Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+          break;
+
+        case HTTP_UPDATE_NO_UPDATES:
+          Serial.println("HTTP_UPDATE_NO_UPDATES");
+          break;
+        // anti bug
+        default:
+          Serial.println("some error (?)");
+          break;
+      }
+    }
+    else {
+      Serial.println( "Already on latest version" );
+    }
+  }
+  else {
+    Serial.print( "Firmware version check failed, got HTTP response code " );
+    Serial.println( httpCode );
+  }
+  httpClient.end();
 }
 
-void printData(){
-  // Imprime na Serial os valores obtidos
-  /* Alterar divisão conforme fundo de escala escolhido:
-      Acelerômetro
-      +/-2g = 16384
-      +/-4g = 8192
-      +/-8g = 4096
-      +/-16g = 2048
-
-      Giroscópio
-      +/-250°/s = 131
-      +/-500°/s = 65.6
-      +/-1000°/s = 32.8
-      +/-2000°/s = 16.4
-  */
-//  if (item == "acc") {
-//    Serial.print(AccX / 2048);
-//    Serial.print(" ");
-//    Serial.print(AccY / 2048);
-//    Serial.print(" ");
-//    Serial.println(AccZ / 2048);
-//  }
-//  if (item == "gyr") {
-    Serial.print(GyrX / 16.4);
-    Serial.print(" ");
-    Serial.print(GyrY / 16.4);
-    Serial.print(" ");
-    Serial.println(GyrZ / 16.4);
-//  }
-}
 
 void setup() {
-  // Inicializa Serial
   Serial.begin(9600);
-  // Inicializa o MPU-6050
-  initMPU();    
-
+  WiFi.mode(WIFI_STA);
+  setWifi();
+  checkForUpdates();
 }
 
 void loop() {
-  readData();
-  printData();
 
-  // Atraso de 100ms
-  delay(100);
 }
--- web_update/src/main.cpp
+++ web_update/src/main.cpp
@@ -1,100 +1,89 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+// #include <ArduinoOTA.h>
+#include <Updater.h>
 #include <ESP8266HTTPClient.h>
-#include <ESP8266httpUpdate.h>
+#include <wifiConnect.h>
+#include <ESP_WiFiManager.h>
 
 
+HTTPClient httpClient;
 WiFiClient client;
-// versao atual do firmware
-const int FW_VERSION = 1244;
-// Url do firmware
-const char* fwUrlBase = "http://192.168.0.1/fota/";
 
-const char* ssid = "Orion*Fibra_David";
-const char* password = "ioner004";
+uint32_t espID = ESP.getChipId();
+int totalLength;
+int currentLength = 0;
 
-void setWifi(){
-  Serial.println();      
-  WiFi.begin(ssid, password); //Inicia WiFi 
-  delay(500);
-  Serial.print("Conectando"); 
-  while (WiFi.status() != WL_CONNECTED) // Tentando conectar na Rede WiFi
-  {
-    delay(500);
-    Serial.print(".");
-   }
-  Serial.println();
-  
-  Serial.print("Conectado | Endereço IP: "); 
-  Serial.println(WiFi.localIP()); //Imprime o Endereço IP Local do ESP8266
+const char* url = "http://storage.googleapis.com/sensor-version/downtime/firmware3.bin";
+
+      
+
+
+void updateFirmware(uint8_t *data, size_t len, String filename){
+  Update.write(data, len);
+  currentLength += len;
+  // Print dots while waiting for update to finish
+  Serial.print('.');
+  // if current length of written firmware is not equal to total firmware size, repeat
+  if(currentLength != totalLength) return;
+  Update.end(true);
+  // APIlog((String)"Updated sucessfully from " + filename);
+  Serial.printf("\nUpdate Success, Total Size: %u\nRebooting in 3s...\n", currentLength);
+  delay(3000);
+  // Restart ESP32 to see changes 
+  ESP.restart();
 }
 
 void checkForUpdates() {
-  String mac = WiFi.macAddress();
-  String fwURL = String( fwUrlBase );
-  fwURL.concat( mac );
-  String fwVersionURL = fwURL;
-  fwVersionURL.concat( ".version" );
 
   Serial.println( "Checking for firmware updates." );
-  Serial.print( "MAC address: " );
-  Serial.println( mac );
-  Serial.print( "Firmware version URL: " );
-  Serial.println( fwVersionURL );
 
-  HTTPClient httpClient;
-  httpClient.begin( client, fwVersionURL );
+  httpClient.begin( client, url );
   int httpCode = httpClient.GET();
+
   if( httpCode == 200 ) {
-    String newFWVersion = httpClient.getString();
-
-    Serial.print( "Current firmware version: " );
-    Serial.println( FW_VERSION );
-    Serial.print( "Available firmware version: " );
-    Serial.println( newFWVersion );
-
-    int newVersion = newFWVersion.toInt();
-
-    if( newVersion > FW_VERSION ) {
-      Serial.println( "Preparing to update" );
-
-      String fwImageURL = fwURL;
-      fwImageURL.concat( ".bin" );
-      // update(client, host (httpserver), port, uri)
-      t_httpUpdate_return ret = ESPhttpUpdate.update( client, "192.168.0.2", 80, "/arduino.bin" );
-
-      switch(ret) {
-        case HTTP_UPDATE_FAILED:
-          Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
-          break;
-
-        case HTTP_UPDATE_NO_UPDATES:
-          Serial.println("HTTP_UPDATE_NO_UPDATES");
-          break;
-        // anti bug
-        default:
-          Serial.println("some error (?)");
-          break;
-      }
+    Serial.print("connected to ");
+    Serial.println(url);
+    // get length of document (is -1 when Server sends no Content-Length header)
+    totalLength = httpClient.getSize();
+    // transfer to local variable
+    int len = totalLength;
+    // this is required to start firmware update process
+    Update.begin(len);
+    Serial.printf("FW Size: %u\n",totalLength);
+    // create buffer for read
+    uint8_t buff[128] = { 0 };
+    // get tcp stream
+    WiFiClient * stream = httpClient.getStreamPtr();
+    // read all data from server
+    Serial.println("Updating firmware...");
+    while(httpClient.connected() && (len > 0 || len == -1)) {
+          // get available data size
+          size_t size = stream->available();
+          if(size) {
+            // read up to 128 byte
+            int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+            // pass to function
+            updateFirmware(buff, c, "firmware.bin");
+            if(len > 0) {
+                len -= c;
+            }
+          }
+          delay(1);
     }
-    else {
-      Serial.println( "Already on latest version" );
-    }
-  }
-  else {
+  } else {
     Serial.print( "Firmware version check failed, got HTTP response code " );
     Serial.println( httpCode );
   }
   httpClient.end();
 }
 
-
 void setup() {
   Serial.begin(9600);
-  setWifi();
+  wifiConnectBegin( "espID" , "123456789" );
   checkForUpdates();
+  // Serial.println("AGORA VAI!!");
+
 }
 
 void loop() {
