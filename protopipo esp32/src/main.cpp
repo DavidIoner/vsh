667277b6ca702f9c26330e27582291d8c0db2fff
DavidIoner <davidhioner@gmail.com>
2022-12-20 02:23:57 -0300

    Automação da manufatura

--- protopipo esp32/src/main.cpp
+++ protopipo esp32/src/main.cpp
@@ -1,249 +1,52 @@
+#include <Arduino.h>
 #include <WiFi.h>
-#include <mDNS.h>
-#include <HTTPClient.h>
-#include <pubSubCLient.h>
-#include <Update.h>
-#include <ArduinoJson.h>
-#include <Wire.h>
+// #include <WiFiManager.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-#include <NTPClient.h>
-#include <ESPDateTime.h>
-#include <WiFiManager.h>
+#include <Arduino_JSON.h>
+#include "SPIFFS.h"
 
+// Replace with your network credentials
+const char* ssid = "david";
+const char* password = "123quatro";
 
+// WiFiManager wifiManager;
 
-//Wifi----------------------
-WiFiManager wifiManager;
-// const char* ssid     = "Orion*Fibra_David";
-// 
-// WS-TISBC
-// WS-SP6
-// const char* password = "ioner004";
-// s@30@ltr&f9wss
-// s@30@ltr&f9ws
-// IPAddress local_IP(192, 168, 3, 254); 
-// Set your Gateway IP address
-// IPAddress gateway(192, 168, 1, 254);
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
-// IPAddress subnet(255, 255, 0, 0);
-// IPAddress primaryDNS(8, 8, 8, 8);   //optional
-// IPAddress secondaryDNS(8, 8, 4, 4); //optional  
-#define rstWifi 23
+// Create an Event Source on /events
+AsyncEventSource events("/events");
 
-//MQTT Broker---------------
-const char *mqtt_broker = "177.71.187.19";
-const char *topic = "sensor.downtime";
-const char *mqtt_username = "david";
-const char *mqtt_password = "JZTeVT5C";
-const int mqtt_port = 1883;
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
 
-//Variaveis-----------------
-bool mqttStatus = 0;
+// Timer variables
+unsigned long lastTime = 0;  
+unsigned long lastTimeTemperature = 0;
+unsigned long lastTimeAcc = 0;
+unsigned long gyroDelay = 10;
+unsigned long temperatureDelay = 1000;
+unsigned long accelerometerDelay = 200;
 
-float vibration;
-float leituras = 0;
-float spectrum;
-//int leitura = 1/30 * 1000;
-bool engineState;
-int stateCount;
-float gyTotal;
+// Create a sensor object
+Adafruit_MPU6050 mpu;
 
-float gyMedia;
-float gyMediaTotal;
+sensors_event_t a, g, temp;
 
 float gyroX, gyroY, gyroZ;
 float accX, accY, accZ;
 float temperature;
 
-float gyroX_temp;
-float gyroY_temp;
-float gyroZ_temp;
-
-//desvio giroscopio---------
-float gyroXerror = 0.05;
+//Gyroscope sensor deviation
+float gyroXerror = 0.07;
 float gyroYerror = 0.03;
 float gyroZerror = 0.01;
 
-//tempo---------------------
-unsigned long lastMs = 0;
-unsigned long ms = millis();
-
-static long long timer = 250;
-
-// static long long pooling = 0;
-// static long long pooling2 = 0;
-
-
-//Objetos-------------------
-sensors_event_t a, g, temp;
-WiFiClient espClient;
-PubSubClient client(espClient);
-Adafruit_MPU6050 mpu;
-
-HTTPClient httpClient;
-HTTPClient apiClient;
-
-
-String espId;
-
-int totalLength;
-int currentLength = 0;
-String sensorId;
-int MAX_REQ_ATTEMPT = 5;
-String API_SERVER = "http://177.71.187.19";
-String BKT_SERVER = "http://plantview.io";
-int API_PORT = 3001;
-const char* url = "http://storage.googleapis.com/sensor-version/downtime/";
-
-void setWifi(){
-  wifiManager.resetSettings();
-  wifiManager.autoConnect("Sensor VSH");
-}
-
-void setupDateTime() {
-
-  // DateTime.setServer("asia.pool.ntp.org");
-  // DateTime.begin(15 * 1000);
-  // from
-  /** changed from 0.2.x **/
-  DateTime.setTimeZone("UTC-0");
-  // this method config ntp and wait for time sync
-  // default timeout is 10 seconds
-  DateTime.begin(/* timeout param */);
-  if (!DateTime.isTimeValid()) {
-    Serial.println("Failed to get time from server.");
-  }
-}
-
-
-String router(String subroute){
-  return BKT_SERVER + (String)":" + API_PORT + subroute;
-}
-
-void APIlog(String msg){
-  String route = "/sensor-log";
-  StaticJsonDocument<1024> docAux;
-  JsonObject reading = docAux.to<JsonObject>();
-  String jsonAux;
-
-  reading["id"] = sensorId;
-  // reading["date"] = getCurrentDateTime();
-  reading["log"] = msg;
-
-  serializeJson(reading, jsonAux);
-
-  apiClient.begin(espClient , router(route));
-  apiClient.addHeader("Content-Type", "application/json");     
-
-  int res = apiClient.POST(jsonAux);
-  apiClient.end();
-  reading.clear();
-}
-
-void generateSensorId() {
-  char espIdAux[12];
-  snprintf(espIdAux, 12, "%X%X", ESP.getEfuseMac());
-  sensorId = espIdAux;
-  Serial.println(sensorId);
-}
-
-void updateFirmware(uint8_t *data, size_t len, String filename){
-  Update.write(data, len);
-  currentLength += len;
-  // Print dots while waiting for update to finish
-  Serial.print('.');
-  // if current length of written firmware is not equal to total firmware size, repeat
-  if(currentLength != totalLength) return;
-  Update.end(true);
-  APIlog((String)"Updated sucessfully from " + filename);
-  Serial.printf("\nUpdate Success, Total Size: %u\nRebooting in 3s...\n", currentLength);
-  delay(3000);
-  // Restart ESP32 to see changes 
-  ESP.restart();
-}
-
-void checkForUpdates() {
-  Serial.println(sensorId);
-  String route = "/check-update?id=" + sensorId + "&downtime=true";
-  Serial.println(route);
-  Serial.println(router(route));
-
-  
-
-  apiClient.begin(espClient , router(route));
-
-  int statusCode = HTTP_CODE_BAD_REQUEST;
-  int attempt = 0;
-
-  while((statusCode != HTTP_CODE_OK) && (attempt < MAX_REQ_ATTEMPT)) {
-    attempt++;
-    statusCode = apiClient.GET();
-  }
-  if(statusCode != HTTP_CODE_OK) {
-
-    // ESP.restart();
-  }
-  String filename = apiClient.getString();
-  apiClient.end();
-
-  if(filename == "firmware16.bin"){
-    Serial.println("Already up to date.");
-    return;
-  }
-
-  String urlf = url + filename;
-
-  Serial.println( "Avaliable." );
-
-  httpClient.begin( espClient, urlf );
-
-  int httpCode = httpClient.GET();
-
-  if( httpCode == 200 ) {
-    Serial.print("connected to ");
-    Serial.println(urlf);
-    // get length of document (is -1 when Server sends no Content-Length header)
-    totalLength = httpClient.getSize();
-    // transfer to local variable
-    int len = totalLength;
-    // this is required to start firmware update process
-    Update.begin(len);
-    Serial.printf("FW Size: %u\n",totalLength);
-    // create buffer for read
-    uint8_t buff[128] = { 0 };
-    // get tcp stream
-    WiFiClient * stream = httpClient.getStreamPtr();
-    // read all data from server
-    Serial.println("Updating firmware...");
-    while(httpClient.connected() && (len > 0 || len == -1)) {
-          // get available data size
-          size_t size = stream->available();
-          if(size) {
-            // read up to 128 byte
-            int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
-            // pass to function
-            updateFirmware(buff, c, filename);
-            if(len > 0) {
-                len -= c;
-            }
-          }
-          delay(1);
-    }
-  } else {
-    Serial.print( "Firmware version check failed, got HTTP response code " );
-    Serial.println( httpCode );
-  }
-  httpClient.end();
-}
-
+// Init MPU6050
 void initMPU(){
-  // 2, 4, 8, 16
-  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-  // 250, 500, 1000, 2000
-  mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
-  // 260, 184, 94, 44, 21, 10, 5
-  mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -253,187 +56,140 @@ void initMPU(){
   Serial.println("MPU6050 Found!");
 }
 
-void printData() {
-  
-      /* Print out the values */
-      
-      Serial.print(accX);
-      Serial.println(" ");
-      Serial.print(accY);
-      Serial.println(" ");
-      Serial.print(accZ);
-      Serial.println(" ");
-    
-      Serial.print(gyroX);
-      Serial.print(" ");
-      Serial.print(gyroY);
-      Serial.print(" ");
-      Serial.print(gyroZ);
-      Serial.println(" ");
-    
-      // Serial.print("Temperature: ");
-      // Serial.print(temp.temperature);
-      // Serial.println(" degC");
-    
-      // Serial.println("");
+void initSPIFFS() {
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  Serial.println("SPIFFS mounted successfully");
 }
 
+// void setWifi(){
+//   wifiManager.autoConnect("Sensor de vibração");
+//   Serial.println(WiFi.localIP());
+// }
 
-void getReadings(){
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+}
+
+String getGyroReadings(){
   mpu.getEvent(&a, &g, &temp);
+
+  float gyroX_temp = g.gyro.x;
+  if(abs(gyroX_temp) > gyroXerror)  {
+    gyroX += gyroX_temp/50.00;
+  }
+  
+  float gyroY_temp = g.gyro.y;
+  if(abs(gyroY_temp) > gyroYerror) {
+    gyroY += gyroY_temp/70.00;
+  }
+
+  float gyroZ_temp = g.gyro.z;
+  if(abs(gyroZ_temp) > gyroZerror) {
+    gyroZ += gyroZ_temp/90.00;
+  }
+
+  readings["gyroX"] = String(gyroX);
+  readings["gyroY"] = String(gyroY);
+  readings["gyroZ"] = String(gyroZ);
+
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+String getAccReadings() {
+  mpu.getEvent(&a, &g, &temp);
+  // Get current acceleration values
   accX = a.acceleration.x;
   accY = a.acceleration.y;
   accZ = a.acceleration.z;
+  readings["accX"] = String(accX);
+  readings["accY"] = String(accY);
+  readings["accZ"] = String(accZ);
+  String accString = JSON.stringify (readings);
+  return accString;
+}
 
+String getTemperature(){
+  mpu.getEvent(&a, &g, &temp);
   temperature = temp.temperature;
-
-  // gyroX = g.gyro.x;
-  // gyroY = g.gyro.y;
-  // gyroZ = g.gyro.z;
-  
-
-  gyroX_temp = g.gyro.x;
-  if(abs(gyroX_temp) >= gyroXerror)  {
-    gyroX = gyroX_temp - gyroXerror;
-  } else{gyroX = 0;}
-  
-  gyroY_temp = g.gyro.y;
-  if(abs(gyroY_temp) >= gyroYerror) {
-    gyroY = gyroY_temp - gyroYerror;
-  } else{gyroY = 0;}
-
-  gyroZ_temp = g.gyro.z;
-  if(abs(gyroZ_temp) >= gyroZerror) {
-    gyroZ = gyroZ_temp - gyroZerror;
-  } else{gyroZ = 0;}
-
-}
-
-void defineState() {
-  gyTotal = abs(gyroX) + abs(gyroY) + abs(gyroZ);
-  if (gyTotal > 0) {
-      stateCount ++;
-  } else if (stateCount){
-    stateCount --;
-    engineState = 0; 
-  }
-  if (stateCount > 4) { //quantidade de leituras para considerar o motor como ligado
-      engineState = 1;
-  } 
-}
-
-float toVibrationSpectrum(){
-  gyMedia = 0;
-  // 5 leituras
-  
-  getReadings();
-  gyTotal = abs(gyroX) + abs(gyroY) + abs(gyroZ);
-  gyMedia += gyTotal;
-
-  return gyMedia;
-}
-
-
-void addToJSON() {
-  StaticJsonDocument<300> docAux;
-  JsonObject reading = docAux.to<JsonObject>();
-  reading["id"] = sensorId;
-  reading["t"] = DateTime.toISOString();
-  reading["v"] = spectrum;
-  // reading["a"] = accX;
-  // reading["b"] = accY;
-  // reading["c"] = accZ;
-
-  // reading["x"] = gyroX;
-  // reading["y"] = gyroY;
-  // reading["z"] = gyroZ;
-
-  char jsonOut[300];
-  serializeJson(docAux, jsonOut);
-  client.publish(topic, jsonOut);
-  reading.clear();
-}
-
-
-
-bool connectMQTT() {
-  byte tentativa = 0;
-  Serial.println("conectando ao broker");
-  client.setServer(mqtt_broker, mqtt_port);
-
-  do {
-    String client_id = "SENSOR01-";
-    client_id += String(WiFi.macAddress());
-
-    if (client.connect(espId.c_str(), mqtt_username, mqtt_password)) {
-      Serial.println("Exito na conexao");
-      Serial.printf("Cliente %s conectado ao broker\n",client_id.c_str());  
-    } else{
-      Serial.print("Falha ao conectar: ");
-      Serial.print(client.state());
-      Serial.println();
-      Serial.print("Tentativa: ");
-      delay(2000);
-    }
-    tentativa++;
-  } while (!client.connected() && tentativa < 5);
-
-  if (tentativa < 5) {
-    return 1;
-  } else {
-    Serial.println("nao conectado");
-    return 0;
-  }
+  return String(temperature);
 }
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(rstWifi, INPUT);
-  setWifi();
-
-  setupDateTime();
-  generateSensorId();
-  checkForUpdates();
-  Serial.println("test");
-  Serial.println("V2");
+  Serial.begin(115200);
+  initWiFi();
+  initSPIFFS();
   initMPU();
-  // client.setCallback(callback);
-  mqttStatus = connectMQTT();
-  if (mqttStatus == 0) {
-    // ESP.reset();
-  }
 
-  // Serial.println("AGORA VAI!!");
+  // Handle Web Server
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
 
+  server.serveStatic("/", SPIFFS, "/");
+
+  server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroX=0;
+    gyroY=0;
+    gyroZ=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetX", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroX=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetY", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroY=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetZ", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroZ=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  // Handle Web Server Events
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+
+  server.begin();
 }
 
 void loop() {
-  static long long pooling = 0;
-  static long long pooling2 = 0;
-  
-  // Serial.println(pooling);
-  if (mqttStatus) {
-    client.loop();
-    if (millis() > pooling + timer) {
-      pooling = millis(); 
-
-      // faz a leitura e retorna um float de vibracao
-      vibration += toVibrationSpectrum();
-      Serial.println(vibration);
-      leituras += 1;
-      if (millis() >= pooling2 + 1000){
-        pooling2 = millis();
-        // faz a media das leituras
-        spectrum = vibration/leituras;
-        addToJSON();
-        vibration = 0;
-        leituras = 0;
-      }
-
-
-        if (WiFi.status() != WL_CONNECTED){
-        //   ESP.reset();
-        }
-        }
-    }
+  if ((millis() - lastTime) > gyroDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getGyroReadings().c_str(),"gyro_readings",millis());
+    lastTime = millis();
   }
+  if ((millis() - lastTimeAcc) > accelerometerDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getAccReadings().c_str(),"accelerometer_readings",millis());
+    lastTimeAcc = millis();
+  }
+  if ((millis() - lastTimeTemperature) > temperatureDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getTemperature().c_str(),"temperature_reading",millis());
+    lastTimeTemperature = millis();
+  }
+}
